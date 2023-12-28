419790d5b3a9e00123af554b5550e8325ebecd4d
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-28 20:54:09 +0800

    java

---
+++ src/day01/ArrayDemo2.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class ArrayDemo2 {
+	public static void main(String[] args) {
+		String[] cars = {"Honda", "Toyota", "Tesla", "BMW", "Benz", "BYD"};
+		String[] type = {"油", "油", "電", "油", "油", "電"};
+		// 寫一個程式可以將
+		// 將油車放到 String[] gasolineCars 陣列中
+		// 將電車放到 String[] electricCars 陣列中
+		// 最後各別印出 gasolineCars 與 electricCars 陣列中的資訊與台數
+		// 印出範例: 油車有 4 台分別是: Honda, Toyota, BMW, Benz 
+		// 印出範例: 電車有 2 台分別是: Tesla, BYD;
+	}
+}
