a57a3d2a36b77bc41bfdd0daaf6bde4b234bdc21
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-02 21:35:16 +0800

    java

---
+++ src/day02/ArrayAndMethod.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class ArrayAndMethod {
+
+	public static void main(String[] args) {
+		int[] scores1 = {100, 90, 80};
+		int[] scores2 = {70, 60, 50};
+		// 請分別計算出總分與平均 ?
+
+	}
+
+}
