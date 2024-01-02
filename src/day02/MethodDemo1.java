3175bf2a0eb076c09425a3938fb2af731e890442
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-02 19:15:07 +0800

    java

--- src/day02/MethodDemo1.java
+++ src/day02/MethodDemo1.java
@@ -41,16 +41,10 @@ public class MethodDemo1 {
 	// 此方法需要 2 個參數, 分別是 h(身高), w(體種)
 	// 此方法執行完後不會有任何回傳值
 	public static void calcBmiAndPrint(double h, double w) {
-		double bmi = w / Math.pow(h/100, 2);
-		String result = "正常";
-		if(bmi > 23) {
-			result = "過重";
-		} else if(bmi <= 18) {
-			result = "過輕";
-		}
+		double bmi = calcBmi(h, w);
+		String result = getResult(bmi);
 		System.out.printf("身高: %.1f 體重: %.1f bmi: %.2f 結果: %s%n", h, w, bmi, result);
 	}
-	
 	
 
 }
