e89d213c96b036aeebe0fb97870252f37e1a1f72
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-02 19:42:48 +0800

    java

---
+++ src/day02/BMIJFrame.java
@@ -0,0 +1,21 @@
+package day02;
+
+/*
+ 視窗版 BMI 計算機
+ 
+ +-----------------+
+ |   BMI Computer  |
+ +-----------------+
+ |  H(cm): ______  |
+ |  W(kg): ______  |
+ |                 |
+ |  [bmi button]   |
+ |                 |
+ |  [value&result] |
+ +-----------------+
+ * */
+
+
+public class BMIJFrame {
+
+}
