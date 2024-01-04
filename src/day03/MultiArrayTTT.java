77f64edcadc6116ad6652fdcae220ab6d62b28ee
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-04 21:12:51 +0800

    java

--- src/day03/MultiArrayTTT.java
+++ src/day03/MultiArrayTTT.java
@@ -24,9 +24,9 @@ public class MultiArrayTTT {
 	public static void printTTT(String[][] ttt) {
 		System.out.println("+-----------+");
 		System.out.println("| " + ttt[0][0] + " | " + ttt[0][1] + " | " + ttt[0][2] + " |");
-		System.out.println("+-----------+");
+		System.out.println("+---+---+---+");
 		System.out.println("| " + ttt[1][0] + " | " + ttt[1][1] + " | " + ttt[1][2] + " |");
-		System.out.println("+-----------+");
+		System.out.println("+---+---+---+");
 		System.out.println("| " + ttt[2][0] + " | " + ttt[2][1] + " | " + ttt[2][2] + " |");
 		System.out.println("+-----------+");
 		
