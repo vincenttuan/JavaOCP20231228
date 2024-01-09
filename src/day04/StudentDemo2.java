424a05a094fc3cfea8c608d8a7823c204150adf4
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-09 19:40:17 +0800

    java

--- src/day04/StudentDemo2.java
+++ src/day04/StudentDemo2.java
@@ -15,8 +15,18 @@ public class StudentDemo2 {
 		// 資料呈現
 		// 自動呼叫每一個陣列元素(student物件)的toString()物件方法
 		System.out.println(Arrays.toString(students)); 
-		
-
+		// 利用 for-loop 將資料呈現
+		for(int i=0;i<students.length;i++) {
+			System.out.println(i + " => " + students[i]);
+		}
+		// 利用 for-each/for-in 將資料呈現
+		for(Student student : students) {
+			System.out.println(student);
+		}
+		// 利用 java 8 stream 串流的 forEach() 簡化寫法 I
+		Arrays.stream(students).forEach(student -> System.out.println(student));
+		// 利用 java 8 stream 串流的 forEach() + :: 方法參考 簡化寫法 II
+		Arrays.stream(students).forEach(System.out::println);
 	}
 
 }
