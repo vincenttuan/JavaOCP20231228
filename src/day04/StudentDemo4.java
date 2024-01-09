1355c332ec207664b969a55c1274285ac20c1de9
vincent tuan <vincentjava@yahoo.com.tw>
2024-01-09 20:58:37 +0800

    java

---
+++ src/day04/StudentDemo4.java
@@ -0,0 +1,15 @@
+package day04;
+
+public class StudentDemo4 {
+	public static void main(String[] args) {
+		Student student1 = new Student("John", 80, 1);
+		Student student2 = new Student("Mary", 70, 2);
+		Student student3 = new Student("Jack", 90, 1);
+		Student student4 = new Student("Rose", 50, 2);
+		Student student5 = new Student("Anna", 43, 2);
+		
+		Student[] students = {student1, student2, student3, student4, student5};
+		// 請將最高分與最低分的人名與分數列出
+		
+	}
+}
