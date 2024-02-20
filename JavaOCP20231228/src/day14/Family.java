9d4be6e918289495ce589f40d864d275ce55240c
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-20 19:58:59 +0800

    java

--- JavaOCP20231228/src/day14/Family.java
+++ JavaOCP20231228/src/day14/Family.java
@@ -44,6 +44,29 @@ public class Family {
 		car.printCar();
 	}
 	
+	// 娛樂活動
+	interface Entertainment {
+		void activity(); // 活動
+	}
 	
+	// 家庭日方法
+	public void familyDay() {
+		Entertainment entertainment1 = new Entertainment() {
+			@Override
+			public void activity() {
+				System.out.println("家庭日-烤肉活動");
+			}
+		};
+		
+		Entertainment entertainment2 = new Entertainment() {
+			@Override
+			public void activity() {
+				System.out.println("家庭日-騎單車活動");
+			}
+		};
+		
+		entertainment1.activity();
+		entertainment2.activity();
+	} 
 	
 }
