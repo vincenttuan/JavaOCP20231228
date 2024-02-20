0a7cba124cf7297f22b6f3957cf8afa407e77509
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-20 21:39:15 +0800

    java

--- JavaOCP20231228/src/day14/BMITest.java
+++ JavaOCP20231228/src/day14/BMITest.java
@@ -1,12 +1,29 @@
 package day14;
 
+import static java.lang.Math.pow;
+
 public class BMITest {
 
 	public static void main(String[] args) {
 		// 利用 Lambda 來實作 BMI
 		// 並計算出 h = 170, w = 60 的 bmi 值 (小數點 2 位)
+		BMI bmi = new BMI() {
+			@Override
+			public double calc(double h, double w) {
+				double result = w / Math.pow(h/100.0, 2);
+				return result;
+			}
+		};
 		
-
+		// Lambda 1
+		BMI bmi1 = (h, w) -> w / Math.pow(h/100.0, 2);
+		
+		// Lambda 2
+		BMI bmi2 = (h, w) -> w / pow(h/100.0, 2);
+		
+		System.out.println(bmi2.calc(170, 60)); // 170.0, 60.0
+		
+		System.out.printf("%.2f%n", bmi2.calc(170, 60));
 	}
 
 }
