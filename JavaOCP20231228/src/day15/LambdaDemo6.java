7a1881fd60d92aa22ed5d9a1d4266eb405883b57
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-27 21:16:09 +0800

    java

--- JavaOCP20231228/src/day15/LambdaDemo6.java
+++ JavaOCP20231228/src/day15/LambdaDemo6.java
@@ -5,7 +5,15 @@ public class LambdaDemo6 {
 		// 混用 Function, Predicate, Consumer
 		String[] scores = {"100", "50", "70", "40", "90"};
 		// 請印出及格的分數
-		
+		for(String str : scores) {
+			// 1.字串轉換成(map)數字
+			int score = Integer.parseInt(str);
+			// 2.判斷過濾(filter)是否及格
+			if(score >= 60) {
+				// 3. 印出
+				System.out.println(score);
+			}
+		}
 		
 		
 		
