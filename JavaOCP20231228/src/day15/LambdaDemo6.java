fdd2909137e659135c21a8f8a735a86dd9939195
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-27 21:06:08 +0800

    java

---
+++ JavaOCP20231228/src/day15/LambdaDemo6.java
@@ -0,0 +1,13 @@
+package day15;
+
+public class LambdaDemo6 {
+	public static void main(String[] args) {
+		// 混用 Function, Predicate, Consumer
+		String[] scores = {"100", "50", "70", "40", "90"};
+		// 請印出及格的分數
+		
+		
+		
+		
+	}
+}
