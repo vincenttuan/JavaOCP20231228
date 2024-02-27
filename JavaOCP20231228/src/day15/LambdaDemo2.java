95a82f1ed3e6e3ddfc61009806dc02ff9c3b789a
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-27 19:32:55 +0800

    java

--- JavaOCP20231228/src/day15/LambdaDemo2.java
+++ JavaOCP20231228/src/day15/LambdaDemo2.java
@@ -12,12 +12,19 @@ public class LambdaDemo2 {
 		}
 		*/
 		
+		// 計算圓面積
 		//Function<Integer, Double> calcArea = (r) -> r * r * 3.14;
 		Function<Integer, Double> calcArea = (r) -> Math.pow(r, 2) * Math.PI;
 		
 		// 計算半徑=10的圓面積
 		Double area = calcArea.apply(10);
 		System.out.println(area);
+		
+		// 計算球體積  
+		Function<Integer, Double> calcVolume = (r) -> 4/3.0 * Math.PI * Math.pow(r, 3);
+		Double volume = calcVolume.apply(10);
+		System.out.println(volume);
+		
 		
 	}
 }
