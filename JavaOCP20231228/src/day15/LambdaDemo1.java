44cea07f3f760e348e4d404cc58f406110b0d437
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-27 19:13:36 +0800

    java

--- JavaOCP20231228/src/day15/LambdaDemo1.java
+++ JavaOCP20231228/src/day15/LambdaDemo1.java
@@ -1,10 +1,27 @@
 package day15;
 
+import java.util.function.Consumer;
+
 public class LambdaDemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 練習 Consumer
+		/*
+		 	@FunctionalInterface
+			public interface Consumer<T> {
+				void accept(T t);
+			}	
+		*/
+		
+		Consumer<String> hello = (t) -> System.out.println("Hello " + t);
+		Consumer<Integer> calc = (t) -> System.out.println(t + t);
+		Consumer test = (t) -> System.out.println(t);
+		
+		hello.accept("John");
+		calc.accept(10);
+		test.accept("abc");
+		test.accept(10);
+		
 	}
 
 }
