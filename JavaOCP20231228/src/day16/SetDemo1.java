91798b912efb9506a0a2d613c31385a15cd25b7a
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-29 19:54:17 +0800

    java

--- JavaOCP20231228/src/day16/SetDemo1.java
+++ JavaOCP20231228/src/day16/SetDemo1.java
@@ -7,8 +7,8 @@ import java.util.Set;
 public class SetDemo1 {
 
 	public static void main(String[] args) {
-		//Set set = new HashSet();      // [80, 英, 65, 韓, 100, 日, 70, 國]
-		Set set = new LinkedHashSet();  // [國, 100, 英, 70, 日, 80, 韓, 65]
+		//Set set = new HashSet();      // [80, 英, 65, 韓, 100, 日, 70, 國] 無序
+		Set set = new LinkedHashSet();  // [國, 100, 英, 70, 日, 80, 韓, 65] 有序
 		set.add("國"); // String
 		set.add(100); // Integer
 		set.add("英");
