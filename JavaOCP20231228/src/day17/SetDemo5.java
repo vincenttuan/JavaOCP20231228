164beb0d59bbbee4e1b46f6e361ae28f9751d121
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-05 19:46:49 +0800

    java

--- JavaOCP20231228/src/day17/SetDemo5.java
+++ JavaOCP20231228/src/day17/SetDemo5.java
@@ -1,5 +1,6 @@
 package day17;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -11,7 +12,18 @@ public class SetDemo5 {
 		set.add(80);
 		set.add(null);
 		set.add(90);
+		System.out.println(set);
 		// 請計算總分
+		// 利用 Iterator 的 remove() 方法將 null 元素從集合中移除
+		Iterator<Integer> iter = set.iterator();
+		while (iter.hasNext()) {
+			if(iter.next() == null) {
+				iter.remove(); // 移除元素
+			}
+		}
+		System.out.println(set);
 		
+		int sum = set.stream().mapToInt(Integer::intValue).sum();
+		System.out.println(sum);
 	}
 }
