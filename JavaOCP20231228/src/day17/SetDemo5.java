65bafc98c2012bcde54738957f51ad8dc7c0b31f
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-05 19:37:50 +0800

    java

---
+++ JavaOCP20231228/src/day17/SetDemo5.java
@@ -0,0 +1,17 @@
+package day17;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo5 {
+	public static void main(String[] args) {
+		Set<Integer> set = new LinkedHashSet<>();
+		set.add(100);
+		set.add(null);
+		set.add(80);
+		set.add(null);
+		set.add(90);
+		// 請計算總分
+		
+	}
+}
