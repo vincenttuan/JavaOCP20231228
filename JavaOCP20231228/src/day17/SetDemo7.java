d0ba4b6698bd1670a36cdc88c4fd81901b6b8c87
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-05 20:49:42 +0800

    java

---
+++ JavaOCP20231228/src/day17/SetDemo7.java
@@ -0,0 +1,17 @@
+package day17;
+
+/**
+調查五位學生之身高及體重如下，試比較其分散程度。
+身高：172、168、164、170、176(公分)
+體重：62、57、58、64、64(公斤)
+比較二者的分散程度哪一個分散程度大 ?
+ * */
+
+public class SetDemo7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
