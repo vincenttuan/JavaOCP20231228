b9b6d10934100aa1fee61944b3ea647b000dff54
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-05 20:59:56 +0800

    java

--- JavaOCP20231228/src/day17/SetDemo7.java
+++ JavaOCP20231228/src/day17/SetDemo7.java
@@ -1,17 +1,27 @@
 package day17;
 
+import java.net.Socket;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.sound.midi.Soundbank;
+
 /**
 調查五位學生之身高及體重如下，試比較其分散程度。
 身高：172、168、164、170、176(公分)
-體重：62、57、58、64、64(公斤)
+體重：62、57、58、63、64(公斤)
 比較二者的分散程度哪一個分散程度大 ?
  * */
 
 public class SetDemo7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Set<Double> heights = new LinkedHashSet<>();
+		heights.add(172.0);heights.add(168.0);heights.add(164.0);heights.add(170.0);heights.add(176.0);
+		Set<Double> weights = new LinkedHashSet<>();
+		weights.add(62.0);weights.add(57.0);weights.add(58.0);weights.add(63.0);weights.add(64.0);
+		System.out.println(heights);
+		System.out.println(weights);
 	}
 
 }
