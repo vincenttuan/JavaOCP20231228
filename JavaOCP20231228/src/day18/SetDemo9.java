7c603fbb78f4607c8a6d5c8540c25d6fd431c536
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-07 19:54:53 +0800

    java

--- JavaOCP20231228/src/day18/SetDemo9.java
+++ JavaOCP20231228/src/day18/SetDemo9.java
@@ -1,5 +1,6 @@
 package day18;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -13,6 +14,18 @@ public class SetDemo9 {
 		set.add(80);
 		System.out.println(set);
 		
+		// 有一系列的飲料
+		Set<Drink> drinks = new TreeSet<>();
+		drinks.add(new Drink("Coffee", 65));
+		drinks.add(new Drink("Tea", 20));
+		drinks.add(new Drink("Milk", 50));
+		drinks.add(new Drink("Cola", 25));
+		drinks.add(new Drink("Cola", 25));
+		drinks.add(new Drink("Beer", 70));
+		drinks.add(new Drink("Beer", 70));
+		drinks.add(new Drink("Yakult", 10));
+		drinks.add(new Drink("Juice", 45));
+		System.out.println(drinks);
 	}
 
 }
