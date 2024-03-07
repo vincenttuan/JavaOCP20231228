9b5f0fa23a58c24bdbfa4f72c7d4f19c25ac499c
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-07 19:37:17 +0800

    java

---
+++ JavaOCP20231228/src/day18/SetDemo9.java
@@ -0,0 +1,18 @@
+package day18;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo9 {
+
+	public static void main(String[] args) {
+		// 排序(預設 小->大)
+		Set<Integer> set = new TreeSet<>();
+		set.add(90);
+		set.add(100);
+		set.add(80);
+		System.out.println(set);
+
+	}
+
+}
