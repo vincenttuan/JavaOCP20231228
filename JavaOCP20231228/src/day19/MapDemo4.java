f4266d844303d56fef6e9c5dacb20ad6f620efa5
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-12 20:45:35 +0800

    java

--- JavaOCP20231228/src/day19/MapDemo4.java
+++ JavaOCP20231228/src/day19/MapDemo4.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class MapDemo4 {
 	public static void main(String[] args) {
-		//List<String> fruits = Arrays.asList("apple", "apple", "banana", "apple", "orange", "banana", "papapy");
-		List<String> fruits = List.of("apple", "apple", "banana", "apple", "orange", "banana", "papapy");
+		//List<String> fruits = Arrays.asList("apple", "apple", "banana", "apple", "orange", "banana", "papaya");
+		List<String> fruits = List.of("apple", "apple", "banana", "apple", "orange", "banana", "papaya"); // 唯讀
 		System.out.println(fruits);
 		// 請問每一種水果個有幾個 ?
 		
