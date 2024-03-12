f0e70d3aee33764cfd581c1fe6702308122c1ce9
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-12 21:19:18 +0800

    java

--- JavaOCP20231228/src/day19/MapDemo5.java
+++ JavaOCP20231228/src/day19/MapDemo5.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class MapDemo5 {
 	public static void main(String[] args) {
 		List<Fruit> fruits = List.of(
-				new Fruit("apple",      10, 9),
-				new Fruit("banana",     20, 19),
+				new Fruit("apple",      10, 9),  // 一箱
+				new Fruit("banana",     20, 19),  
 				new Fruit("orange",     10, 29),
 				new Fruit("watermelon", 10, 29),
 				new Fruit("papaya",     20, 9),
@@ -15,6 +15,7 @@ public class MapDemo5 {
 				new Fruit("apple",      20, 9)
 		);
 		System.out.println(fruits);
+		// 每種水果各有幾箱 ?
 		
 	}
 }
