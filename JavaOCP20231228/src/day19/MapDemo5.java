b90b18558d646e997b6ec48ed1cc61bba705ecbe
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-12 21:39:45 +0800

    java

--- JavaOCP20231228/src/day19/MapDemo5.java
+++ JavaOCP20231228/src/day19/MapDemo5.java
@@ -25,7 +25,22 @@ public class MapDemo5 {
 				}, Collectors.counting()));
 		System.out.println(result1);
 		
+		Map<String, Long> result2 = fruits.stream().collect(
+				Collectors.groupingBy(fruit -> fruit.getName(), Collectors.counting()));
+		System.out.println(result2);
 		
+		Map<String, Long> result3 = fruits.stream().collect(
+				Collectors.groupingBy(Fruit::getName, Collectors.counting()));
+		System.out.println(result3);
+		
+		// 每種水果各有幾顆 ?
+		Map<String, Integer> result4 = fruits.stream().collect(
+				Collectors.groupingBy(Fruit::getName, Collectors.summingInt(fruit -> fruit.getQty())));
+		System.out.println(result4);
+		
+		Map<String, Integer> result5 = fruits.stream().collect(
+				Collectors.groupingBy(Fruit::getName, Collectors.summingInt(Fruit::getQty)));
+		System.out.println(result5);
 		
 	}
 }
