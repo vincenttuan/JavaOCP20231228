7dc2c6c4ea7b4bbe75dbcb042d2aff7a13fc40b3
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-12 19:47:15 +0800

    java

---
+++ JavaOCP20231228/src/day19/MapDemo3.java
@@ -0,0 +1,25 @@
+package day19;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo3 {
+	public static void main(String[] args) {
+		Map<String, Integer> exams = new LinkedHashMap<>();
+		exams.put("國文", 100);
+		exams.put("英文", 90);
+		exams.put("社會", 85);
+		exams.put("自然", 30);
+		exams.put("數學", 90);
+		
+		// 等級 數量
+		//  A: 3
+		//  B: 1
+		//  C: 0
+		//  D: 0
+		//  F: 1
+		
+		
+		
+	}
+}
