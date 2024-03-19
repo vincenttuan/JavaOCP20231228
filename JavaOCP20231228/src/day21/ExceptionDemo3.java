6af78b7df5086da55c14bea3a4506e0a0a70e35e
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-19 19:54:35 +0800

    java

---
+++ JavaOCP20231228/src/day21/ExceptionDemo3.java
@@ -0,0 +1,36 @@
+package day21;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionDemo3 {
+
+	public static void main(String[] args) {
+		while (true) {
+			if(input()) {
+				break;
+			}
+		}
+	}
+	
+	public static boolean input() {
+		
+		try(Scanner scanner = new Scanner(System.in)) { // try-with-resource 啟動自動關閉
+			System.out.print("請輸入分子:");
+			int x = scanner.nextInt();
+			System.out.print("請輸入分母:");
+			int y = scanner.nextInt();
+			int result = x / y;
+			System.out.printf("result = %d%n", result);
+			return true;
+		} catch	(InputMismatchException e1) {
+			System.out.println("輸入錯誤, 錯誤原因:" + e1);
+			System.out.println("請重新輸入");
+		} catch (ArithmeticException e2) {
+			System.out.println("計算錯誤, 錯誤原因:" + e2);
+			System.out.println("請重新輸入");
+		}
+		return false;
+	}
+
+}
