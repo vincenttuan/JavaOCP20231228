f6729e4413389355a7d2703fe2d3429b6bbea62e
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-19 19:36:12 +0800

    java

--- JavaOCP20231228/src/day21/ExceptionDemo1.java
+++ JavaOCP20231228/src/day21/ExceptionDemo1.java
@@ -1,5 +1,6 @@
 package day21;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo1 {
@@ -10,12 +11,18 @@ public class ExceptionDemo1 {
 	
 	public static void input() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("請輸入分子:");
-		int x = scanner.nextInt();
-		System.out.print("請輸入分母:");
-		int y = scanner.nextInt();
-		int result = x / y;
-		System.out.printf("result = %d%n", result);
+		try {
+			System.out.print("請輸入分子:");
+			int x = scanner.nextInt();
+			System.out.print("請輸入分母:");
+			int y = scanner.nextInt();
+			int result = x / y;
+			System.out.printf("result = %d%n", result);
+		} catch	(InputMismatchException e) {
+			System.out.println("分子輸入錯誤, 錯誤原因:" + e);
+			System.out.println("請重新輸入");
+			input(); // 重新執行
+		}
 	}
 
 }
