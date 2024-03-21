8f6343ccc9ec797a9cd5fed489eadc8085a0a433
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-21 21:27:19 +0800

    java

--- JavaOCP20231228/src/day22/ScoreDao.java
+++ JavaOCP20231228/src/day22/ScoreDao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class ScoreDao {
@@ -42,6 +43,30 @@ public class ScoreDao {
 			System.out.println("資料庫錯誤: " + e);
 		}
 		return false;
+	}
+	
+	// 建立 score 資料表
+	private Boolean createScoreTable() {
+		String sql = "create table if not exists score ( "
+				+ "	id int not null auto_increment, "
+				+ " name varchar(255) not null unique, "
+				+ " chinese int not null default 0, "
+				+ " english int not null default 0, "
+				+ " math int not null default 0, "
+				+ " sum int not null default 0, "
+				+ " avg double not null default 0.0, "
+				+ " primary key(id) "
+				+ ");";
+		try(Connection conn = DriverManager.getConnection(URL, DB_USERNAME, DB_PASSWORD);
+			Statement stmt = conn.createStatement()) {
+			
+			stmt.executeUpdate(sql);
+			
+		} catch (SQLException e) {
+			System.out.println("資料庫錯誤: " + e);
+			return false;
+		}
+		return true;
 	}
 	
 }
