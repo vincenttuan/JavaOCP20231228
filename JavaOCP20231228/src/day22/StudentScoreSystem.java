b651eda8877eeb5dd34f579b9b1d9a398cbcf261
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-21 19:30:27 +0800

    java

--- JavaOCP20231228/src/day22/StudentScoreSystem.java
+++ JavaOCP20231228/src/day22/StudentScoreSystem.java
@@ -1,9 +1,15 @@
 package day22;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class StudentScoreSystem {
 	public static void main(String[] args) {
+		StudentScoreSystem system = new StudentScoreSystem();
 		// 1. 讀取檔案(score.csv)
-		
+		system.readFile("src\\day22\\output\\score.csv");
 		// 2. 分析內容
 		
 		// 3. 輸出分析內容(result.csv)
@@ -15,5 +21,17 @@ public class StudentScoreSystem {
 	}
 	
 	// 1. 讀取檔案(score.csv)
-	
+	public List<Score> readFile(String filePath) {
+		String content = null;
+		try {
+			content = Files.readString(Path.of(filePath));
+		} catch (IOException e) {
+			System.out.println("IO 錯誤: " + e);
+			return null; // 回傳空值表示無資料
+		}	
+		// 印出 content
+		System.out.println(content);
+		
+		return null;
+	} 
 }
