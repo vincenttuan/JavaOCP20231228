b10f79e704afc5467ad111dd231b7fbe21398a68
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-21 19:18:39 +0800

    java

---
+++ JavaOCP20231228/src/day22/StudentScoreSystem.java
@@ -0,0 +1,16 @@
+package day22;
+
+public class StudentScoreSystem {
+	public static void main(String[] args) {
+		// 1. 讀取檔案(score.csv)
+		
+		// 2. 分析內容
+		
+		// 3. 輸出分析內容(result.csv)
+		
+		// 4. 登入程序
+		
+		// 5. 批次匯入(result.csv)
+		
+	}
+}
