c248a2e73edc0d6e48a3530bde87dba2530ca5af
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-26 20:46:55 +0800

    java

--- JavaOCP20231228/src/day23/ThreadDemo3.java
+++ JavaOCP20231228/src/day23/ThreadDemo3.java
@@ -11,6 +11,11 @@ public class ThreadDemo3 {
 		Total total = new Total(); // 再加上一個 Total 執行緒物件 (不分奇數與偶數)
 		System.out.printf("執行緒名字: %s, 指派 Even, Odd 與 Total 來執行任務%n", tName);
 		
+		// 設定執行緒名字
+		even.setName("T1");
+		odd.setName("T2");
+		total.setName("T3");
+		
 		even.start(); // 開始執行任務
 		odd.start();  // 開始執行任務
 		total.start(); // 開始執行任務
