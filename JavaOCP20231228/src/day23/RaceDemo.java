5b8824c203a06c4f36e56dd720c0a0b43b868c9b
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-26 21:10:25 +0800

    java

--- JavaOCP20231228/src/day23/RaceDemo.java
+++ JavaOCP20231228/src/day23/RaceDemo.java
@@ -5,8 +5,13 @@ public class RaceDemo {
 	public static void main(String[] args) {
 		Race tortoise = new Race();
 		Race rabbit = new Race();
+		// 設定名字
 		tortoise.setName("烏龜");
 		rabbit.setName("兔子");
+		// 設定權限
+		tortoise.setPriority(10);
+		rabbit.setPriority(1);
+		
 		System.out.println("比賽開始");
 		
 		tortoise.start();
