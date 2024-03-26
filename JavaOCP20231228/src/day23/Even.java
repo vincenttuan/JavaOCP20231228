213af7eb12f603dc1dac959a48d2e10d8597d965
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-26 19:45:26 +0800

    java

---
+++ JavaOCP20231228/src/day23/Even.java
@@ -0,0 +1,15 @@
+package day23;
+
+public class Even {
+	private void job() {
+		String tName = Thread.currentThread().getName();
+		// 任務一: 1~1000 的所有偶數並逐項把累計資料印出
+		int sumOfEven = 0;
+		for(int i=1;i<=1000;i++) {
+			if(i % 2 == 0) {
+				sumOfEven += i;
+				System.out.printf("執行緒名字: %s, 偶數 i = %d 累計總和 = %d%n", tName, i, sumOfEven);
+			}
+		}
+	}
+}
---
+++ JavaOCP20231228/src/day23/Odd.java
@@ -0,0 +1,15 @@
+package day23;
+
+public class Odd {
+	private void job() {
+		String tName = Thread.currentThread().getName();
+		// 任務二: 1~1000 的所有奇數並逐項把累計資料印出
+		int sumOfOdd = 0;
+		for(int i=1;i<=1000;i++) {
+			if(i % 2 == 1) {
+				sumOfOdd += i;
+				System.out.printf("執行緒名字: %s, 奇數 i = %d 累計總和 = %d%n", tName, i, sumOfOdd);
+			}
+		}
+	}
+}
