a225386634f3124d8f2ce0494b16bf332714fd52
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-26 21:05:28 +0800

    java

---
+++ JavaOCP20231228/src/day23/RaceDemo.java
@@ -0,0 +1,17 @@
+package day23;
+
+public class RaceDemo {
+
+	public static void main(String[] args) {
+		Race turtle = new Race();
+		Race rabbit = new Race();
+		turtle.setName("烏龜");
+		rabbit.setName("兔子");
+		System.out.println("比賽開始");
+		
+		turtle.start();
+		turtle.start();
+
+	}
+
+}
