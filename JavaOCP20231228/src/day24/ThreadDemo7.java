f3596575a077e3b4cc5df4c0b908e26b38103dc2
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 21:22:34 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo7.java
+++ JavaOCP20231228/src/day24/ThreadDemo7.java
@@ -1,9 +1,12 @@
 package day24;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class ThreadDemo7 {
 
@@ -12,7 +15,18 @@ public class ThreadDemo7 {
 			try {
 				String url = "https://tw.stock.yahoo.com/quote/2330";
 				Document doc = Jsoup.connect(url).get();
-				System.out.println(doc);
+				// 找到 <ul> 下的 <li class="price-detail-item"> 前面幾個字是 price-detail-item 的元素
+				Elements elements = doc.select("ul > li.price-detail-item");
+				// 過濾 html tag
+				Map<String, String> priceMap = new HashMap<>();
+				elements.forEach(e -> {
+					// e 裡面有 2 個 <span> 分別印出 text
+					Elements spans = e.select("span");
+					String key = spans.get(0).text().replace(" ", "").replace("\n", "");
+					String value = spans.get(1).text().replace("%", "").replace(",", "").replace(" ", "").replace("\n", "");
+					priceMap.put(key, value);
+				});
+				System.out.println(priceMap);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
