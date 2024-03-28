220bf96d86d8f4b16bc614be71d64c1961e27abd
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 20:05:15 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo5.java
+++ JavaOCP20231228/src/day24/ThreadDemo5.java
@@ -2,6 +2,15 @@ package day24;
 
 public class ThreadDemo5 {
 	public static void main(String[] args) {
+		Runnable job1 = () -> {
+			Fibonacci fibonacci = new Fibonacci();
+			long n = 10;
+			long result = fibonacci.fib(n);
+			System.out.printf("fibonacci[%d] = %d%n", n, result);
+		};
+		
+		Thread t1 = new Thread(job1);
+		t1.start();
 		
 		
 	}
