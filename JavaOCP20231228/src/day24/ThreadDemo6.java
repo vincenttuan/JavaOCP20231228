7eacc425693524e0c039e72392824afe5b1fa04f
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 20:53:22 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo6.java
+++ JavaOCP20231228/src/day24/ThreadDemo6.java
@@ -7,12 +7,19 @@ public class ThreadDemo6 {
 		// 檔案列表
 		List<String> filePaths = List.of("src/day24/score1.txt", "src/day24/score2.txt", "src/day24/score3.txt");
 		
+		// 分時多工
 		filePaths.forEach(filePath -> {
 			CalcRunnable calc = new CalcRunnable(filePath);
 			Thread t = new Thread(calc);
 			t.start();
 		});
 		
+		// 平行
+		filePaths.parallelStream().forEach(filePath -> {
+			CalcRunnable calc = new CalcRunnable(filePath);
+			Thread t = new Thread(calc);
+			t.start();
+		});
 		
 	}
 }
