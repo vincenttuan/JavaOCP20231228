a7cb7f8b79706745b208165e721eacc4122b7872
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 20:08:31 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo5.java
+++ JavaOCP20231228/src/day24/ThreadDemo5.java
@@ -1,12 +1,16 @@
 package day24;
 
+import java.util.Date;
+
 public class ThreadDemo5 {
 	public static void main(String[] args) {
 		Runnable job1 = () -> {
+			long startTime = new Date().getTime();
 			Fibonacci fibonacci = new Fibonacci();
-			long n = 10;
+			long n = 50;
 			long result = fibonacci.fib(n);
-			System.out.printf("fibonacci[%d] = %d%n", n, result);
+			long endTime = new Date().getTime();
+			System.out.printf("fibonacci[%d] = %d time: %d%n", n, result, (endTime - startTime));
 		};
 		
 		Thread t1 = new Thread(job1);
