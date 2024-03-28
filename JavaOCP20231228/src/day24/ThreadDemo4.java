2a35699bea091e46648f6392da4d3f130a9ba9d3
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 19:38:38 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo4.java
+++ JavaOCP20231228/src/day24/ThreadDemo4.java
@@ -2,6 +2,7 @@ package day24;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 public class ThreadDemo4 {
@@ -22,6 +23,14 @@ public class ThreadDemo4 {
 			System.out.println("台積電股價: 750");
 		};
 		
+		Thread t1 = new Thread(job1); // 建立一個 t1 執行緒來做 job1 的工作
+		Thread t2 = new Thread(job2); // 建立一個 t2 執行緒來做 job2 的工作
+		Thread t3 = new Thread(job3); // 建立一個 t3 執行緒來做 job3 的工作
+		Thread t4 = new Thread(job1); // 建立一個 t4 執行緒來做 job1 的工作
+		Thread t5 = new Thread(job3); // 建立一個 t5 執行緒來做 job3 的工作
+		
+		List<Thread> threads = List.of(t1, t2, t3, t4, t5);
+		threads.forEach(t -> t.start());
 		
 	}
 
