688a4175038b8c8f42119414ef27ce5611900c2f
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-28 21:27:51 +0800

    java

--- JavaOCP20231228/src/day24/ThreadDemo7.java
+++ JavaOCP20231228/src/day24/ThreadDemo7.java
@@ -2,6 +2,7 @@ package day24;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.jsoup.Jsoup;
@@ -13,12 +14,14 @@ public class ThreadDemo7 {
 	public static void main(String[] args) {
 		Runnable crawler = () -> {
 			try {
-				String url = "https://tw.stock.yahoo.com/quote/2330";
+				String symbol = Thread.currentThread().getName();
+				String url = "https://tw.stock.yahoo.com/quote/" + symbol;
 				Document doc = Jsoup.connect(url).get();
 				// 找到 <ul> 下的 <li class="price-detail-item"> 前面幾個字是 price-detail-item 的元素
 				Elements elements = doc.select("ul > li.price-detail-item");
 				// 過濾 html tag
-				Map<String, String> priceMap = new HashMap<>();
+				Map<String, String> priceMap = new LinkedHashMap<>();
+				priceMap.put("代號", symbol);
 				elements.forEach(e -> {
 					// e 裡面有 2 個 <span> 分別印出 text
 					Elements spans = e.select("span");
@@ -32,8 +35,10 @@ public class ThreadDemo7 {
 			}
 		};
 		
-		Thread t = new Thread(crawler);
-		t.start();
+		Thread t1 = new Thread(crawler, "2330");
+		t1.start();
+		Thread t2 = new Thread(crawler, "2317");
+		t2.start();
 
 	}
 
