ed70e3ec255386f4fe3a6073fa8c85dd32d0050b
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-02 20:07:51 +0800

    java

---
+++ JavaOCP20231228/src/day25/ThreadDemo9.java
@@ -0,0 +1,32 @@
+package day25;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadDemo9 {
+	public static void main(String[] args) {
+		Account account = new Account(10000);
+		
+		// 使用執行緒服務 + stream
+		// 工作列表
+		List<Runnable> tasks = List.of(
+				new Withdraw(account, 5000),
+				new Withdraw(account, 4000),
+				new Withdraw(account, 3000),
+				new Deposit(account, 2000),
+				new Deposit(account, 1000)
+		);
+		
+		// 使用 CachedThreadPool 或 newFixedThreadPool 管理執行緒
+		ExecutorService exector = Executors.newFixedThreadPool(5);
+		
+		// 使用 Stream API 提交任務
+		tasks.stream().forEach(task -> exector.execute(task));
+		
+		// 任務完成關閉服務
+		exector.shutdown();
+		
+	}
+}
