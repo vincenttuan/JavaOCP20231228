5d51c8f793ddd29871de430af506c04b05f5ace8
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-02 21:04:07 +0800

    java

--- JavaOCP20231228/src/day25/Account.java
+++ JavaOCP20231228/src/day25/Account.java
@@ -8,37 +8,43 @@ public class Account {
 	}
 	
 	// 提款方法, account 金額
-	public synchronized void withdraw(int amount) {
+	public void withdraw(int amount) {
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 進來提款 $%d, 帳戶餘額 $%d%n", tName, amount, balance);
 		
-		// 提款程序
-		if(balance >= amount) {
-			// 進行提款
-			// 模擬提款所花費的時間
-			for(int i=0;i<=999999;i++);
-			
-			// 帳戶扣抵
-			balance = balance - amount;
-			
-			System.out.printf("%s 提款 $%d 成功, 帳戶餘額 $%d%n", tName, amount, balance);
-		} else {
-			System.out.printf("%s 提款 $%d 失敗, 帳戶餘額 $%d%n", tName, amount, balance);
+		synchronized (this) {
+			// 提款程序
+			if(balance >= amount) {
+				// 進行提款
+				// 模擬提款所花費的時間
+				for(int i=0;i<=999999;i++);
+				
+				// 帳戶扣抵
+				balance = balance - amount;
+				
+				System.out.printf("%s 提款 $%d 成功, 帳戶餘額 $%d%n", tName, amount, balance);
+			} else {
+				System.out.printf("%s 提款 $%d 失敗, 帳戶餘額 $%d%n", tName, amount, balance);
+			}
 		}
 	}
 	
 	// 存款
-	public synchronized void deposit(int amount) {
+	public void deposit(int amount) {
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 進來存款 $%d, 帳戶餘額 $%d%n", tName, amount, balance);
-		// 進行存款
-		// 模擬存款所花費的時間
-		for(int i=0;i<=999999;i++);
 		
-		// 帳戶增額
-		balance = balance + amount;
+		synchronized(this) {
+			// 進行存款
+			// 模擬存款所花費的時間
+			for(int i=0;i<=999999;i++);
+			
+			// 帳戶增額
+			balance = balance + amount;
+			
+			System.out.printf("%s 存款 $%d 成功, 帳戶餘額 $%d%n", tName, amount, balance);
+		}
 		
-		System.out.printf("%s 存款 $%d 成功, 帳戶餘額 $%d%n", tName, amount, balance);
 	}
 	
 }
