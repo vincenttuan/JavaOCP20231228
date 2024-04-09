d329e588e4b92eb8b5712c0828ebbd01eee1538e
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-09 20:58:53 +0800

    java

--- JavaOCP20231228/src/day26/coffee/Producer.java
+++ JavaOCP20231228/src/day26/coffee/Producer.java
@@ -1,5 +1,31 @@
 package day26.coffee;
 
-public class Producer {
+// 生產者
+public class Producer implements Runnable {
+	
+	// 訊息對列
+	private MessageQueue queue;
+	
+	// 咖啡庫存
+	private int coffeeStock = 10;
+	
+	public Producer(MessageQueue queue) {
+		this.queue = queue;
+	}
 
+	@Override
+	public void run() {
+		System.out.println("生產者 run()");
+		System.out.println("生產前庫存:" + coffeeStock);
+		// 模擬生產咖啡
+		queue.produce("拿鐵咖啡");
+		// 庫存更新
+		coffeeStock--;
+		System.out.println("生產後庫存:" + coffeeStock);
+	}
+	
+	public int getCoffeeStock() {
+		return coffeeStock;
+	}
+	
 }
