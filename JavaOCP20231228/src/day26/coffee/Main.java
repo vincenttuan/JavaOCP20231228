e02696db2969b0440735cb4426098bff1f2a4f3b
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-09 21:26:43 +0800

    java

--- JavaOCP20231228/src/day26/coffee/Main.java
+++ JavaOCP20231228/src/day26/coffee/Main.java
@@ -4,10 +4,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		MessageQueue queue = new MessageQueue();
 		Producer producer = new Producer(queue);
-		Consumer consumer = new Consumer(queue);
+		Consumer consumer1 = new Consumer(queue);
+		Consumer consumer2 = new Consumer(queue);
 		
 		// 啟動消費者執行緒
-		new Thread(consumer).start(); // 消費啟動
+		new Thread(consumer1, "小明").start(); // 消費啟動
+		new Thread(consumer2, "小英").start(); // 消費啟動
 		
 		// 生產者不斷生產咖啡
 		while(true) {
