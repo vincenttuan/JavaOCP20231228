57e02036b919e6350a1b81199d55d1a747be1289
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-09 21:24:47 +0800

    java

--- JavaOCP20231228/src/day26/coffee/Consumer.java
+++ JavaOCP20231228/src/day26/coffee/Consumer.java
@@ -10,12 +10,13 @@ public class Consumer implements Runnable {
 	
 	@Override
 	public void run() {
-		System.out.println("消費者 run()");
+		String threadName = Thread.currentThread().getName();
+		System.out.printf("消費者 %s run()%n", threadName);
 		// 消費者想買 10 杯咖啡
 		for(int i=1;i<=10;i++) {
 			String data = queue.consume(); // 從對列中取出一杯咖啡
 			// 消費 data
-			System.out.printf("消費者喝了第 %d 杯 %s%n", i, data);
+			System.out.printf("消費者 %s 喝了第 %d 杯 %s%n", threadName, i, data);
 		}
 	}
 
