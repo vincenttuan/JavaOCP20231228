d5b38ac6804d01cf40e405b45204f3214d23998d
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-09 20:54:11 +0800

    java

--- JavaOCP20231228/src/day26/coffee/MessageQueue.java
+++ JavaOCP20231228/src/day26/coffee/MessageQueue.java
@@ -1,5 +1,35 @@
 package day26.coffee;
 
-public class MessageQueue {
+import java.util.LinkedList;
+import java.util.Queue;
 
+// 訊息對列
+public class MessageQueue {
+	
+	// 存放訊息的集合
+	private Queue<String> queue = new LinkedList<>();
+	
+	// 生產
+	public synchronized void produce(String message) {
+		System.out.println("生產者將 " + message + " 放到對列中");
+		queue.add(message); // 將新的訊息放到對列中
+		notify(); // 通知消費者進行消費
+	}
+	
+	// 消費
+	public synchronized String consume(String message) {
+		// 查驗 queue 是否有資料
+		if(queue.isEmpty()) {
+			System.out.println("消費者等待中...");
+			try {
+				wait(); // 等待生產者的通知
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		String data = queue.poll(); // 將資料取出
+		return data;
+	}
+	
+	
 }
