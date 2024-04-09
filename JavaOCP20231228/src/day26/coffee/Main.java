63868d8648398bcab421ed701c5e48274419e634
MB-teacher <MB-teacher@MB-207-teacher>
2024-04-09 21:13:25 +0800

    java

--- JavaOCP20231228/src/day26/coffee/Main.java
+++ JavaOCP20231228/src/day26/coffee/Main.java
@@ -1,5 +1,19 @@
 package day26.coffee;
 
 public class Main {
-
+	public static void main(String[] args) throws Exception {
+		MessageQueue queue = new MessageQueue();
+		Producer producer = new Producer(queue);
+		Consumer consumer = new Consumer(queue);
+		
+		// 啟動消費者執行緒
+		new Thread(consumer).start(); // 消費啟動
+		
+		// 生產者不斷生產咖啡
+		while(true) {
+			Thread.sleep(3000);
+			new Thread(producer).start(); // 生產啟動
+		}
+		
+	}
 }
